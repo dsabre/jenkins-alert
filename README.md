20521952fbc924f0ff23d45bef5a0de2ccb5cca2
Daniele Sabre <daniele.sabre@gmail.com>
2023-07-24 11:35:24 +0200

    Develop

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ A simple Jenkins watcher in python
 
 ## Usage
 
-Create a script inside the scripts directory with the following code:
+Create a script inside the scripts directory with the following code (try the versions to find what fits to you):
+
+Old version
 
 ```bash
 #!/bin/bash
@@ -29,4 +31,24 @@ TELEGRAM_CHAT_ID="" # optional
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../main.py"
 
 python3 "$SCRIPT_PATH" "$JENKINS_PROJECT" "$JENKINS_PROMOTION_STAGE_NAME" "$JENKINS_PROMOTION_PROD_NAME" "$JENKINS_URL" "$JENKINS_USERNAME" "$JENKINS_PASSWORD" $SLEEP_TIME "$TELEGRAM_BOT_TOKEN" "$TELEGRAM_CHAT_ID"
+```
+
+New version:
+
+```bash
+#!/bin/bash
+
+# script configuration
+JENKINS_PROJECT="project-name|split-by-pipes"
+JENKINS_URL="http://your-jenkins-url.example"
+JENKINS_USERNAME="username"
+JENKINS_PASSWORD="password"
+SLEEP_TIME=5 # info refresh timing expressed in seconds
+TELEGRAM_BOT_TOKEN="" # optional
+TELEGRAM_CHAT_ID="" # optional
+# end script configuration
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../new.py"
+
+python3 "$SCRIPT_PATH" "$JENKINS_PROJECT" "$JENKINS_URL" "$JENKINS_USERNAME" "$JENKINS_PASSWORD" $SLEEP_TIME "$TELEGRAM_BOT_TOKEN" "$TELEGRAM_CHAT_ID"
 ```
---
+++ new.py
@@ -0,0 +1,86 @@
+import requests, sys, os, time, subprocess;
+from tabulate import tabulate
+
+JENKINS_PROJECT = sys.argv[1].split("|")
+JENKINS_URL = sys.argv[2]
+JENKINS_USERNAME = sys.argv[3]
+JENKINS_PASSWORD = sys.argv[4]
+SLEEP_TIME = float(sys.argv[5])
+TELEGRAM_BOT_TOKEN = sys.argv[6]
+TELEGRAM_CHAT_ID = sys.argv[7]
+REQUESTS_TIMEOUT=10
+
+continueCheck = True
+showNotification = False
+
+class bcolors:
+    HEADER = '\033[95m'
+    OK = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    INFO = '\033[96m'
+    GRAY = '\033[37m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def show_error_from_url(url):
+    print(f"{bcolors.FAIL}Error from url: {bcolors.UNDERLINE}{url}{bcolors.END}")
+    sys.exit(1)
+
+def do_jenkins_request(url):
+    try:
+        response = requests.get(url, auth=(JENKINS_USERNAME, JENKINS_PASSWORD), timeout=REQUESTS_TIMEOUT)
+        if response.status_code != 200:
+            show_error_from_url(url)
+        
+        return response
+    except:
+        show_error_from_url(url)
+
+def get_building_string(build_data):
+    return (f'{bcolors.WARNING}yes{bcolors.END}') if build_data['building'] else 'no'
+
+def get_result(build_data):
+    if build_data['building']:
+        return f'{bcolors.WARNING}BUILDING{bcolors.END}'
+    
+    return (f'{bcolors.OK}{build_data["result"]}{bcolors.END}' if build_data['result'] == 'SUCCESS' else f'{bcolors.FAIL}{build_data["result"]}{bcolors.END}')
+
+def do_telegram_request(text):
+    if TELEGRAM_BOT_TOKEN == '' or TELEGRAM_CHAT_ID == '':
+        return False
+
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+    return requests.post(url, json={'chat_id': TELEGRAM_CHAT_ID, 'text': text}, timeout=REQUESTS_TIMEOUT).status_code == 200
+
+while continueCheck:
+    lastBuildUrl = do_jenkins_request(f'{JENKINS_URL}/job/{"/job/".join(JENKINS_PROJECT)}/api/json').json()['lastBuild']['url']
+    buildData = do_jenkins_request(f'{lastBuildUrl}api/json').json()
+
+    tableRows = [['Build', buildData['id'], get_building_string(buildData), get_result(buildData)]]
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'{bcolors.HEADER}Project:{bcolors.BOLD} {" > ".join(JENKINS_PROJECT)}{bcolors.END}')
+    print('')
+    print(f'{bcolors.GRAY}URLs:{bcolors.END}')
+    print(f'{bcolors.GRAY}({bcolors.UNDERLINE}{JENKINS_URL}{bcolors.END}{bcolors.GRAY})\n({bcolors.UNDERLINE}{JENKINS_URL}/job/{"/job/".join(JENKINS_PROJECT)}{bcolors.END}{bcolors.GRAY}){bcolors.END}')
+    print('')
+    print(tabulate(tableRows, headers=[
+        f'{bcolors.BOLD}Operation{bcolors.END}',
+        f'{bcolors.BOLD}ID{bcolors.END}',
+        f'{bcolors.BOLD}Running{bcolors.END}',
+        f'{bcolors.BOLD}Status{bcolors.END}'
+        ], tablefmt='rounded_grid'))
+    print('')
+
+    continueCheck = buildData['building']
+
+    if continueCheck:
+        showNotification = True
+        time.sleep(SLEEP_TIME)
+
+if showNotification:
+    message = f'Jenkins for {" > ".join(JENKINS_PROJECT)} is ended'
+    subprocess.Popen(['notify-send', message])
+    do_telegram_request(message)
